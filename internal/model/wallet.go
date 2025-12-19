bc92494d28c5b4ac70ceb492db2c0bd135bdbda6
Ibrahim <Selmentausen@gmail.com>
2025-12-19 09:47:08 +0300

    init project structure and define models

---
+++ internal/model/wallet.go
@@ -0,0 +1,21 @@
+package model
+
+import "github.com/google/uuid"
+
+type OperationType string
+
+const (
+	Deposit  OperationType = "DEPOSIT"
+	Withdraw OperationType = "WITHDRAW"
+)
+
+type WalletRequest struct {
+	WalletID      uuid.UUID     `json:"walletId" binding:"required"`
+	OperationType OperationType `json:"operationType" binding:"required,oneof=DEPOSIT WITHDRAW"`
+	Amount        float64       `json:"amount" binding:"required,gt=0"`
+}
+
+type WalletResponse struct {
+	WalletID uuid.UUID `json:"Id"`
+	Balance  float64   `json:"balance"`
+}
